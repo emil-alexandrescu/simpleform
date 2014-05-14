6750909714e0330f0c9a747e51063bd327de55f3
Emil Alexandrescu <emil.alexandresku@gmail.com>
2014-05-14 21:10:42 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Simpleform::Application.config.secret_token = '173acaddc910163ded9b714ad85ee3ba9862f8f42c27a42c85659df2bf5f50ce6c9388750e886835ddbd876d68efeb15bbd3eb287168d90e7ff43a5b9575cd20'
